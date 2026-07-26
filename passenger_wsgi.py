fe82f71fa34455deb241db708752000bec819959
Pasechneek <danil.merkurev@yandex.com>
2026-07-26 16:18:35 +0300

    passenger_wsgi add

---
+++ passenger_wsgi.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os, sys
+sys.path.insert(0, '/var/www/u3376679/data/my_simple_rent/')
+sys.path.insert(1, '/var/www/u3376679/data/my_simple_rent/.venv/lib/python3.12')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_simple_rent.settings'
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
