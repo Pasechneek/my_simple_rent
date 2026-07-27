c6e5382d615e52c9e33de368cea7b8138158912f
Pasechneek <danil.merkurev@yandex.com>
2026-07-27 22:29:05 +0300

    fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u3376679/data/www/my-simple-rent.ru/my_simple_rent/')
-sys.path.insert(1, '/var/www/u3376679/data/www/my-simple-rent.ru/my_simple_rent/.venv/lib/python3.12')
+sys.path.insert(0, '/var/www/u3376679/data/www/my-simple-rent.ru/')
+sys.path.insert(1, '/var/www/u3376679/data/www/my-simple-rent.ru/.venv/lib/python3.12/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'my_simple_rent.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
